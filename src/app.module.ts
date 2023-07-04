4e099bb5134056b63a101797e82deca19a25dec8
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-04 14:02:11 -0300

    feat(state): add getAllState endpoint to retrieve all states
    Implement StateService to retrieve states from the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StateModule } from './state/state.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     UserModule,
+    StateModule,
     ConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
     }),
