396d1cec01f2cad3df99edc1f3f90c8bc1f391b7
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-07 09:50:12 -0300

    feat(address): createAddress method with user and city validation
    This commit enhances the createAddress method by incorporating additional validation steps to ensure
    the integrity and correctness of the user and city data before saving the address.

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityModule } from '../city/city.module';
+import { UserModule } from '../user/user.module';
+import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entity/address.entity';
-import { AddressController } from './address.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  imports: [TypeOrmModule.forFeature([AddressEntity]), UserModule, CityModule],
   controllers: [AddressController],
   providers: [AddressService],
 })
--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CityService } from '../city/city.service';
+import { UserService } from '../user/user.service';
 import { CreateAddressDto } from './DTO/create-address.dto';
 import { AddressEntity } from './entity/address.entity';
 
@@ -9,11 +11,15 @@ export class AddressService {
   constructor(
     @InjectRepository(AddressEntity)
     private readonly addressRepository: Repository<AddressEntity>,
+    private readonly userService: UserService,
+    private readonly cityService: CityService,
   ) {}
   async createAddress(
     createAddressDTO: CreateAddressDto,
     userId: number,
   ): Promise<AddressEntity> {
+    await this.userService.getUserById(userId);
+    await this.cityService.getCityById(createAddressDTO.cityId);
     return this.addressRepository.save({
       ...createAddressDTO,
       userId,
