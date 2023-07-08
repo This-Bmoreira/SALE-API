620f083621c814ab4a3e529e94b33abec09635b8
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-08 07:58:59 -0300

    feat(dto): update ReturnAddressDTO with optional city property
    Updated the ReturnAddressDTO class to include an optional city property. The DTO now contains
    properties such as complement, numberAddress, and cep for representing address data. Additionally,
    if available, the DTO includes an instance of ReturnCityDTO to represent the associated city.

--- src/address/DTO/return-address.dto.ts
+++ src/address/DTO/return-address.dto.ts
@@ -1,13 +1,16 @@
+import { ReturnCityDTO } from '../../city/DTO/return-city-dto';
 import { AddressEntity } from '../entity/address.entity';
 
 export class ReturnAddressDTO {
   complement: string;
   numberAddress: number;
   cep: string;
+  city: ReturnCityDTO;
   constructor(address: AddressEntity) {
-    const { complement, cep, numberAddress } = address;
+    const { complement, cep, numberAddress, city } = address;
     this.complement = complement;
     this.numberAddress = numberAddress;
     this.cep = cep;
+    this.city = city ? new ReturnCityDTO(city) : undefined;
   }
 }
