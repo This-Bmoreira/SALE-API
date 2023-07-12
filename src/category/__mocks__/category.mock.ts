22d939419a5e6db1406c79014f276f53cd95c525
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-12 09:43:37 -0300

    feat(category): add CategoryEntity and categoryMock
    The CategoryEntity class defines the structure of the "category" entity in the database. It includes
    columns such as id, name, createdAt, and updatedAt. It also establishes a one-to-many relationship
    with the ProductEntity class.The categoryMock object is a mock data instance of the CategoryEntity
    class. It includes sample values for createdAt, updatedAt, id, and name.

---
+++ src/category/__mocks__/category.mock.ts
@@ -0,0 +1,8 @@
+import { CategoryEntity } from '../entity/category.entity';
+
+export const categoryMock: CategoryEntity = {
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  id: 2,
+  name: 'categoryMock',
+};
