864631787d6085acd62ac4bfcb01e710a3d4d660
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-13 10:40:12 -0300

    feat(product): add 'id' property to ReturnProduct class constructor

--- src/product/DTO/return-product.dto.ts
+++ src/product/DTO/return-product.dto.ts
@@ -2,11 +2,13 @@ import { ProductEntity } from '../entity/product.entity';
 
 export class ReturnProduct {
   categoryId: number;
+  id: number;
   name: string;
   price: number;
   image: string;
-  constructor({ categoryId, name, price, image }: ProductEntity) {
+  constructor({ categoryId, name, price, image, id }: ProductEntity) {
     this.categoryId = categoryId;
+    this.id = id;
     this.name = name;
     this.price = price;
     this.image = image;
