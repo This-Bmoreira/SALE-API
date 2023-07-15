725ec10c2c0b2ce94d8056bcd5f48f3b537c7bd6
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-15 08:32:52 -0300

    feat: add ProductService dependency to CartProductService
    The ProductService is necessary for the new functionality introduced in the insertProductInCart
    method. Before adding a product to the cart, this method now calls the findProductById method from
    the ProductService to ensure that the product exists. If the product is not found, an exception will
    be thrown.

--- src/cart-product/cart-product.module.ts
+++ src/cart-product/cart-product.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartProductService } from './cart-product.service';
 import { CartProductEntity } from './entity/cart-product.entity';
+import { productModule } from '../product/product.module';
 
 @Module({})
 @Module({
-  imports: [TypeOrmModule.forFeature([CartProductEntity])],
+  imports: [TypeOrmModule.forFeature([CartProductEntity]), productModule],
   providers: [CartProductService],
   exports: [CartProductService],
 })
