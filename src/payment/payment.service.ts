f9830de97f91c8be2bb33205073fdf0b29d03120
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-20 15:22:05 -0300

    refactor(payment): update createPayment for price calculation
    The original version of the createPayment method only handled payment entity creation based on the
    createOrderDTO. The refactored version now includes additional functionality to calculate the
    finalPrice based on the products in the cart and handle the creation of specific payment entities
    accordingly.

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,8 +1,11 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CartProductEntity } from '../cart-product/entity/cart-product.entity';
+import { CartEntity } from '../cart/entity/cart.entity';
 import { CreateOrderDTO } from '../order/DTO/create-order.dto';
 import { PaymentType } from '../payment-status/enum/payment-type.enum';
+import { ProductEntity } from '../product/entity/product.entity';
 import { PaymentCreditCardEntity } from './entity/payment-credit-card.entity';
 import { PaymentPixEntity } from './entity/payment-pix.entity';
 import { PaymentEntity } from './entity/payment.entity';
@@ -14,22 +17,38 @@ export class PaymentService {
     private readonly paymentRepository: Repository<PaymentEntity>,
   ) {}
 
-  async createPayment(createOrderDTO: CreateOrderDTO): Promise<PaymentEntity> {
+  async createPayment(
+    createOrderDTO: CreateOrderDTO,
+    products: ProductEntity[],
+    cart: CartEntity,
+  ): Promise<PaymentEntity> {
+    const finalPrice = cart.cartProduct
+      ?.map((cartProduct: CartProductEntity) => {
+        const product = products.find(
+          (product) => product.id === cartProduct.productId,
+        );
+        if (product) {
+          return cartProduct.amount * product.price;
+        }
+        return 0;
+      })
+      .reduce((acc, currentValue) => acc + currentValue, 0);
+
     if (createOrderDTO.amountPayments) {
       const paymentCreditCard = new PaymentCreditCardEntity(
         PaymentType.Done,
+        finalPrice,
         0,
-        0,
-        0,
+        finalPrice,
         createOrderDTO,
       );
       return this.paymentRepository.save(paymentCreditCard);
     } else if (createOrderDTO.codePix && createOrderDTO.datePayment) {
       const paymentPix = new PaymentPixEntity(
         PaymentType.Done,
+        finalPrice,
         0,
-        0,
-        0,
+        finalPrice,
         createOrderDTO,
       );
       return this.paymentRepository.save(paymentPix);
@@ -38,7 +57,5 @@ export class PaymentService {
     throw new BadRequestException(
       'Amount Payments or code pix or date payment not found',
     );
-
-    return;
   }
 }
