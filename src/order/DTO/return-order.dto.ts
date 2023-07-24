ba6c02818000614439852c0797301e8e41775dcf
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-24 11:18:46 -0300

    feat: add ReturnOrderDTO with extended properties
    - Expanded the ReturnOrderDTO class to include additional properties and nested DTOs.
    - The
    ReturnOrderDTO now includes 'userId', 'addressId', 'paymentId' properties to represent the
    respective IDs associated with the order.
    - Added 'address' and 'payment' properties as nested DTOs
    to represent the address and payment information associated with the order.
    - Introduced the
    'ordersProduct' property as an array of nested DTOs to represent the order products associated with
    the order.

--- src/order/DTO/return-order.dto.ts
+++ src/order/DTO/return-order.dto.ts
@@ -1,3 +1,6 @@
+import { ReturnAddressDTO } from '../../address/DTO/return-address.dto';
+import { ReturnOrderProductDTO } from '../../order-product/DTO/return-order-product.dto';
+import { ReturnPaymentDTO } from '../../payment/DTO/return-payment.dto';
 import { ReturnUserDTO } from '../../user/DTO/return-user.dto';
 import { OrderEntity } from '../entity/order.entity';
 
@@ -5,10 +8,30 @@ export class ReturnOrderDTO {
   id: number;
   date: string;
   user?: ReturnUserDTO;
+  ordersProduct?: ReturnOrderProductDTO[];
+  userId: number;
+  addressId: number;
+  paymentId: number;
+  address?: ReturnAddressDTO;
+  payment?: ReturnPaymentDTO;
 
   constructor(order?: OrderEntity) {
     this.id = order?.id;
     this.date = order?.date.toString();
     this.user = order?.user ? new ReturnUserDTO(order?.user) : undefined;
+    this.userId = order?.userId;
+    this.addressId = order?.addressId;
+    this.paymentId = order?.paymentId;
+    this.address = order?.address
+      ? new ReturnAddressDTO(order?.address)
+      : undefined;
+    this.payment = order?.payment
+      ? new ReturnPaymentDTO(order?.payment)
+      : undefined;
+    this.ordersProduct = order?.ordersProduct
+      ? order?.ordersProduct.map(
+          (orderProduct) => new ReturnOrderProductDTO(orderProduct),
+        )
+      : undefined;
   }
 }
