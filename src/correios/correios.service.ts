f5165bf7e951a62744a50dd234d837a3df0e3881
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-07-26 13:08:38 -0300

    feat: implement priceDelivery method in CorreiosService
    - Implemented the priceDelivery method in CorreiosService to calculate shipping costs and delivery
    estimates.
    - The method uses the SOAP API 'CalcPrecoPrazo' to perform the calculation and retrieve
    the response.
    - Provided relevant parameters such as cdService, cep, and sizeProduct (a DTO
    representing the product dimensions and value).
    - Resolves the Promise with the fetched response if
    successful, or throws a BadRequestException in case of errors.

--- src/correios/correios.service.ts
+++ src/correios/correios.service.ts
@@ -1,19 +1,26 @@
 import { HttpService } from '@nestjs/axios';
 import {
   BadRequestException,
+  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { AxiosError } from 'axios';
+import { Client } from 'nestjs-soap';
 import { CityService } from '../city/city.service';
 import { CityEntity } from '../city/entity/city.entity';
+import { ResponsePriceCorreios } from '../order-product/DTO/response-price-correios';
 import { ReturnCepExternal } from './DTO/return-cep-external.dto';
 import { ReturnCep } from './DTO/return-cep.dto';
+import { SizeProductDTO } from './DTO/size-product.dto';
+import { CdFormatEnum } from './enum/cd-format.enum';
 
 @Injectable()
 export class CorreiosService {
+  CEP_COMPANY = process.env.CEP_COMPANY;
   urlCorreios = process.env.URL_CEP_CORREIO;
   constructor(
+    @Inject('SOAP_CORREIOS') private readonly soapClient: Client,
     private readonly httpService: HttpService,
     private readonly cityService: CityService,
   ) {}
@@ -38,5 +45,39 @@ export class CorreiosService {
       .catch(() => undefined);
 
     return new ReturnCep(returnCep, city?.id, city?.state?.id);
+  }
+  async priceDelivery(
+    cdService: string,
+    cep: string,
+    sizeProduct: SizeProductDTO,
+  ): Promise<ResponsePriceCorreios> {
+    return new Promise((resolve, rejects) => {
+      this.soapClient.CalcPrecoPrazo(
+        {
+          nCdServico: cdService,
+          sCepOrigem: this.CEP_COMPANY,
+          sCepDestino: cep,
+          nCdFormato: CdFormatEnum.BOX,
+          nVlPeso: sizeProduct.weight,
+          nVlComprimento: sizeProduct.length,
+          nVlAltura: sizeProduct.height,
+          nVlLargura: sizeProduct.width,
+          nVlDiametro: sizeProduct.diameter,
+          nCdEmpresa: '',
+          sDsSenha: '',
+          sCdMaoPropria: 'N',
+          nVlValorDeclarado:
+            sizeProduct.productValue < 25 ? 0 : sizeProduct.productValue,
+          sCdAvisoRecebimento: 'N',
+        },
+        (_, res: ResponsePriceCorreios) => {
+          if (res) {
+            resolve(res);
+          } else {
+            throw new BadRequestException('Error SOAP ');
+          }
+        },
+      );
+    });
   }
 }
